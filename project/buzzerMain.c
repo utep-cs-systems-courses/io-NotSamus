21d68335fcf4d66c0c7f0bf5e33efff142e96ca3
NotSamus <jlopez126@miners.utep.edu>
2023-11-08 13:37:44 -0700

    this is the last update from this lab

--- project/buzzerMain.c
+++ project/buzzerMain.c
@@ -4,15 +4,15 @@
 #include "lcdutils.h"
 #include "lcddraw.h"
 
-int redrawScreen =1;
 
-#define BG_COLOR COLOR_RED
+#define BG_COLOR COLOR_BLACK
 
 int main(){
     configureClocks();
-    lcd_init();	 
-    clearScreen(BG_COLOR);
+    lcd_init();
     buzzer_init();
+
+    clearScreen(BG_COLOR);
     buzzer_set_period(2000);
 
     or_sr(0x18);
